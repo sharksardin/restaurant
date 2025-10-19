aecffb0783a3cc9c1c46fbbeb32849c397dea18d
sharksardin <4874rezzo@gmail.com>
2025-10-19 03:13:57 +0000

    주변맛집 찾기

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  /* config options here */
+  output: 'export',                 // ✅ 'standalone' 대신 'export'
+  basePath: '/restaurant',          // ✅ GitHub Pages repo 이름과 맞춰야 함
+  assetPrefix: '/restaurant/',  // 중요: 정적 자원 경로 보정
+  images: { unoptimized: true },    // ✅ GitHub Pages에서 이미지 최적화 못 쓰니까 필요
 };
 
 export default nextConfig;
